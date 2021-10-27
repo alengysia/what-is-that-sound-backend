dc0ff2f5e4d5b00f9f9dbfa0a95a6465ee8f487a
Ashley Swank <Ashley@Psychics-MacBook-Pro.local>
2021-10-26 22:38:27 -0400

    WIP fix for Index Route

--- server.js
+++ server.js
@@ -26,12 +26,10 @@ app.use(express.json());
 app.use(cors())
 
 
-app.get("/", (req, res) => {
-    res.send("hello world");
-  });
 
 
-app.use("/instrument", instrumentsController)
+
+app.use("/", instrumentsController)
 
 
 
