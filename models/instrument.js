4c94d823f060f981acea08aeb6e12247bdb2868a
Ashley Swank <Ashley@Psychics-MacBook-Pro.local>
2021-10-28 17:42:47 -0400

    change to Schema

--- models/instrument.js
+++ models/instrument.js
@@ -9,7 +9,7 @@ const instrumentSchema = new Schema ({
     instTradition: {type: Boolean, required: true},
     instAbout: {type: String, required: true},
     instVid: {type: String, required: true},
-    author:{type: Schema.Types.ObjectId, ref: "User"}
+    uid:{type: Schema.Types.ObjectId, ref: "User"}
 });
 
 const Instruments = mongoose.model("Instruments", instrumentSchema)
