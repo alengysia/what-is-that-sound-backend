949c6910474043643f5d2e9b75ae96075242e753
Ashley Swank <Ashley@Psychics-MacBook-Pro.local>
2021-10-29 16:52:19 -0400

    Temp change to Schema for testing

--- models/instrument.js
+++ models/instrument.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const instrumentSchema = new Schema ({
-    instName: {type: String, required: true},
-    instType: {type: String, required: true},
-    instImage: {type: String, required: true},
-    instOrigin: {type: String, required: true},
+    instName: {type: String},
+    instType: {type: String},
+    instImage: {type: String},
+    instOrigin: {type: String},
     instTradition: {type: Boolean},
-    instAbout: {type: String, required: true},
-    instVid: {type: String, required: true},
+    instAbout: {type: String},
+    instVid: {type: String},
     uid:{type: String},
 });
 
