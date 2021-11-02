61ad5cd903dad2ca29f1e1b626cfd460b949f480
Ashley Swank <Ashley@Psychics-MacBook-Pro.local>
2021-11-02 18:57:48 -0400

    add all the routes

--- controllers/landing.js
+++ controllers/landing.js
@@ -13,46 +13,6 @@ router.get('/', async (req, res) => {
 });
 
 
-router.delete('/:id', async (req, res) => {
-    try {
-      // send all people
-      res.json(await Instruments.findByIdAndDelete(req.params.id));
-    } catch (error) {
-      //send error
-      res.status(400).json(error);
-    }
-  });
-
-router.put("/:id", async (req, res) => {
-    try {
-        res.json(
-            await Instruments.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        );
-    } catch (error) {
-        res.status(400).json(error);
-    }
-});
-
-
-
-
-router.post('/', async (req, res) =>{
-    try {
-        res.json(await Instruments.create(req.body));
-    } catch (error) {
-        res.status(400).json(error)
-    }
-});
-
-router.get("/:id", async (req, res) => {
-    try {
-        // send all people
-        res.json(await Instruments.findById(req.params.id));
-      } catch (error) {
-        //send error
-        res.status(400).json(error);
-      }
-    });
 
 
 module.exports = router
